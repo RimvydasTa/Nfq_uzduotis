ca90494809576d0edebb9f64a151408d17fe7f53
Rimvydas Tamošiūnas <rimvydas.tamosiunas@unishop.lt>
2018-09-17 16:42:17 +0300

    order view structure change and hidden input add

--- views/orders.view.php
+++ views/orders.view.php
@@ -4,46 +4,49 @@
 
 $pages = $_SESSION['pages'] ?? '';
 
-var_dump($pages);
-
 ?>
-<form action="?page=1" method="post">
+<form action="" method="post">
     <input class="input" type="text" name="searchString">
     <button name= "searchOrder" class="is-primary button is-medium">Search</button>
-</form>
+
+    <input name="search-inp" class="search-inp" type="text" readonly hidden value="">
+    <input name="sortBy-inp" class="sortBy-inp" type="text" readonly hidden value="id">
+    <input name="sort-inp" class="sort-inp" type="text" readonly hidden value="ASC">
+    <input name="page" class="page" type="text" readonly hidden value="1">
+
 <table class="table">
   <thead>
     <tr>
       <th>Id
-              <a href="?sort=ASC&sortBy=id"> <i class="fa fa-angle-up"></i></a>
-              <a href="?sort=DESC&sortBy=id"> <i class="fa fa-angle-down"></i></a>
+              <a href="" data-sortBy="id" data-sort="ASC"> <i class="fa fa-angle-up"></i></a>
+              <a href="" data-sortBy="id" data-sort="DESC"> <i class="fa fa-angle-down"></i></a>
       <th>First Name
-                <a href="?sort=ASC&sortBy=first_name"> <i class="fa fa-angle-up"></i></a>
-                <a href="?sort=DESC&sortBy=first_name"> <i class="fa fa-angle-down"></i></a>
+                <a href="" data-sortBy="first_name" data-sort="ASC"> <i class="fa fa-angle-up"></i></a>
+                <a href="" data-sortBy="first_name" data-sort="DESC"> <i class="fa fa-angle-down"></i></a>
             </th>
       <th>Last Name
-              <a href="?sort=ASC&sortBy=last_name"> <i class="fa fa-angle-up"></i></a>
-              <a href="?sort=DESC&sortBy=last_name"> <i class="fa fa-angle-down"></i></a>
+              <a href="" data-sortBy="last_name" data-sort="ASC"> <i class="fa fa-angle-up"></i></a>
+              <a href="" data-sortBy="last_name" data-sort="DESC"> <i class="fa fa-angle-down"></i></a>
           </th>
       <th>Email
-              <a href="?sort=ASC&sortBy=email"> <i class="fa fa-angle-up"></i></a>
-              <a href="?sort=DESC&sortBy=email"> <i class="fa fa-angle-down"></i></a>
+              <a href="" data-sortBy="email" data-sort="ASC"> <i class="fa fa-angle-up"></i></a>
+              <a href="" data-sortBy="email" data-sort="DESC"> <i class="fa fa-angle-down"></i></a>
           </th>
       <th>Address
-              <a href="?sort=ASC&sortBy=address"> <i class="fa fa-angle-up"></i></a>
-              <a href="?sort=DESC&sortBy=address"> <i class="fa fa-angle-down"></i></a>
+              <a href="" data-sortBy="address" data-sort="ASC"> <i class="fa fa-angle-up"></i></a>
+              <a href="" data-sortBy="address" data-sort="DESC"> <i class="fa fa-angle-down"></i></a>
           </th>
       <th>Phone
-              <a href="?sort=ASC&sortBy=phone"> <i class="fa fa-angle-up"></i></a>
-              <a href="?sort=DESC&sortBy=phone"> <i class="fa fa-angle-down"></i></a>
+              <a href="" data-sortBy="phone" data-sort="ASC"> <i class="fa fa-angle-up"></i></a>
+              <a href="" data-sortBy="phone" data-sort="DESC"> <i class="fa fa-angle-down"></i></a>
           </th>
       <th>Quantity
-              <a href="?sort=ASC&sortBy=quantity"> <i class="fa fa-angle-up"></i></a>
-              <a href="?sort=DESC&sortBy=quantity"> <i class="fa fa-angle-down"></i></a>
+              <a href="" data-sortBy="quantity" data-sort="ASC"> <i class="fa fa-angle-up"></i></a>
+              <a href="" data-sortBy="quantity" data-sort="DESC"> <i class="fa fa-angle-down"></i></a>
           </th>
       <th>Order date
-              <a href="?sort=ASC&sortBy=date"> <i class="fa fa-angle-up"></i></a>
-              <a href="?sort=DESC&sortBy=date"> <i class="fa fa-angle-down"></i></a>
+              <a href="" data-sortBy="date" data-sort="ASC"> <i class="fa fa-angle-up"></i></a>
+              <a href="" data-sortBy="date" data-sort="DESC"> <i class="fa fa-angle-down"></i></a>
           </th>
     </tr>
   </thead>
@@ -66,9 +69,9 @@ var_dump($pages);
   </tbody>
 </table>
     <?php for($x = 1; $x <= $pages; $x++): ?>
-        <a class="pagination-link" href="?page=<?php echo $x;?>"><?php echo $x ?></a>
+        <a data-page="<?php echo $x ?>" class="pagination-link" href=""><?php echo $x ?></a>
 
     <?php endfor; ?>
 
-
+</form>
 <?php include "footer.view.php" ?>
